a5be5ae6553a31e115a00e247315924adcf55592
teezrus <teezkit@ya.ru>
2021-08-17 16:18:51 +0300

    added promo end partners

--- app/js/main.js
+++ app/js/main.js
@@ -13,4 +13,26 @@ $(function () {
       ratedFill: "#ffc35b",
       readOnly: true,
    });
+
+   const deadlineDate = new Date($('.promo__clock').attr('data-time')).getTime();
+   const countdownDays = document.querySelector('.promo__clock-days .promo__number');
+   const countdownHours = document.querySelector('.promo__clock-hours .promo__number');
+   const countdownMinutes = document.querySelector('.promo__clock-minutes .promo__number');
+   const countdownSeconds = document.querySelector('.promo__clock-seconds .promo__number');
+
+   setInterval(() => {
+      const currentDate = new Date().getTime();
+      const distance = deadlineDate - currentDate;
+
+      const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+      countdownDays.innerHTML = days;
+      countdownHours.innerHTML = hours;
+      countdownMinutes.innerHTML = minutes;
+      countdownSeconds.innerHTML = seconds;
+   }, 1000);
+
 })
